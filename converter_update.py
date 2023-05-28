15480d0438fed5ebd55fea3ef795cde22e23fdb0
arihantvyavhare <arihantljain.vfx@gmail.com>
2023-05-29 00:13:26 +0530

    added # %% for cell detection

--- converter_update.py
+++ converter_update.py
@@ -1,6 +1,9 @@
 import json
 import argparse
 
+#below command for conversion 
+# -> python -u "/home/demonhunter/projects/device_detector/converter_update.py" 'tips.ipynb' 
+
 parser = argparse.ArgumentParser(description='Convert .ipynb file to .py file')
 parser.add_argument('input_file', help='Path to the input .ipynb file')
 args = parser.parse_args()
@@ -13,11 +16,13 @@ py_file = open(f"{filename}.py", "w+")
 for cell in code['cells']:
     if cell['cell_type'] == 'code':
         if cell['source'] and "#| export" in cell['source'][0]:
+            py_file.write("# %%\n")       #you can comment out this line
             for line in cell['source'][1:]:
                 py_file.write(line)
         py_file.write("\n")
     elif cell['cell_type'] == 'markdown':
         py_file.write("\n")
+        py_file.write("# %%\n")         #you can comment out this line
         for line in cell['source']:
             if line and line[0] == "#":
                 py_file.write(line)
--- tips.py
+++ tips.py
@@ -1,9 +1,11 @@
 
+# %%
 # Tips:
 ## downloading images using duckduckgo and fastbook
 #* 
 #* test
 
+# %%
 from fastbook import *
 
 query = "joker"
@@ -13,8 +15,10 @@ urls = search_images_ddg(query, max_images = 5 )
 len(urls)
 
 # for url in urls : print(url)
+# %%
 for i,url in enumerate(urls):
     download_url(url,f"data/{query}/{query}_{i+1}.jpg")
+# %%
 import shutil 
 
 shutil.rmtree(f"data/{query}")
